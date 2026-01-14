8b14fa709ef9d91ca99063a28b8455f900144079
ValentinCA28 <12286@holbertonstudents.com>
2026-01-14 11:41:02 +0100

    task 11 push push

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Compute a to the power of b.
+
+    Args:
+        a: The base number.
+        b: The exponent.
+
+    Returns:
+        The value of a raised to the power of b.
+    """
+    return a ** b
