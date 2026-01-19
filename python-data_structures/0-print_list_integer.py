b0a87ceb924e7f3c1139fba92554b41bc0b42a95
ValentinCA28 <12286@holbertonstudents.com>
2026-01-19 09:11:58 +0100

    task 0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+def print_list_integer(my_list=[]):
+    """Print all integers of a list.
+
+    Args:
+        my_list: The list of integers to print.
+    """
+    for i in my_list:
+        print("{:d}".format(i))
