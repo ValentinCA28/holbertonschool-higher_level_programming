aa8137cd043877de0da774f6ebf0681484f77b44
ValentinCA28 <12286@holbertonstudents.com>
2026-01-21 14:54:52 +0100

    task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides element by element two lists.
+
+    Args:
+        my_list_1: First list containing integers or floats.
+        my_list_2: Second list containing integers or floats.
+        list_length: Number of elements to divide.
+
+    Returns:
+        A new list with all divisions.
+    """
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+    return new_list
