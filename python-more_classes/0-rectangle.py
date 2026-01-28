8e6128e546d58d72a9359449cef44f1ee80e2640
ValentinCA28 <12286@holbertonstudents.com>
2026-01-28 09:45:15 +0100

    task 0 + readme + gitignore

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    An empty class that represents a rectangle.
+
+    This class serves as a foundation for future rectangle implementations.
+    """
+
+    pass
