5293faea8368fdaa864b861b23ff36e27c167ba4
ValentinCA28 <12286@holbertonstudents.com>
2026-02-02 21:30:58 +0100

    task 8 + implement import from prev tasks on task 6 & 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,29 +2,26 @@
 """Module that defines functions and classes."""
 
 
-BaseGeometry = __import__('6-base_geometry').BaseGeometry
+class BaseGeometry:
+    def area(self):
+        """Function description.
 
+        Returns:
+            Return value description.
+        """
+        raise Exception("area() is not implemented")
 
-def area(self):
-    """Function description.
+    def integer_validator(self, name, value):
+        """Function description.
 
-    Returns:
-        Return value description.
-    """
-    raise Exception("area() is not implemented")
+        Args:
+            name: Description of name.
+            value: Description of value.
 
-
-def integer_validator(self, name, value):
-    """Function description.
-
-    Args:
-        name: Description of name.
-        value: Description of value.
-
-    Returns:
-        Return value description.
-    """
-    if not isinstance(value, int):
-        raise TypeError("{} must be an integer".format(name))
-    if value <= 0:
-        raise ValueError("{} must be greater than 0".format(name))
+        Returns:
+            Return value description.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
