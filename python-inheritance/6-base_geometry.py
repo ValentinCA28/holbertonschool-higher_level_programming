1b30355124087cd5c5a24e921a94fa64719d2475
ValentinCA28 <12286@holbertonstudents.com>
2026-02-02 21:08:51 +0100

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines functions and classes."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Function description.
+
+        Returns:
+            Return value description.
+        """
+        raise Exception("area() is not implemented")
