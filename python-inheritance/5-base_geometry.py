f7761c863ec729ae699543bcdd438da4d0d98f79
ValentinCA28 <12286@holbertonstudents.com>
2026-02-04 00:26:54 +0100

    task 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module that defines functions and classes."""
+"""Module that defines the BaseGeometry class."""
 
 
 class BaseGeometry:
+    """An empty base geometry class."""
+
     pass
