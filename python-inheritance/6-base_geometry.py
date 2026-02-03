f0a14381446ee9673359204bc3e4bb948650ae66
ValentinCA28 <12286@holbertonstudents.com>
2026-02-04 00:23:07 +0100

    task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module that defines functions and classes."""
+"""Module that defines the BaseGeometry class."""
 
 
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class BaseGeometry:
+    """A base class for geometry operations."""
 
+    def area(self):
+        """Calculate the area of the geometry.
 
-def area(self):
-    """Function description.
-
-        Returns:
-            Return value description.
+        Raises:
+            Exception: Always raises an exception indicating area() is not
+                implemented.
         """
-    raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
