275c5f8235dcee44c99a95ca8800486d7a9b6f8d
ValentinCA28 <12286@holbertonstudents.com>
2026-02-09 15:14:24 +0100

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines functions and classes."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Function description.
+
+    Args:
+        my_obj: Description of my_obj.
+
+    Returns:
+        Return value description.
+    """
+    return json.dumps(my_obj)
