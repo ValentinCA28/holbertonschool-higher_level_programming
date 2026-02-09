6fbd5c71eccb474a5d98d84076d6ff867c34b439
ValentinCA28 <12286@holbertonstudents.com>
2026-02-09 15:25:27 +0100

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines functions and classes."""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Function description.
+
+    Args:
+        my_str: Description of my_str.
+
+    Returns:
+        Return value description.
+    """
+    return json.loads(my_str)
