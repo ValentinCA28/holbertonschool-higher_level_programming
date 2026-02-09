ff5a71a1e42eb306ffe9198b720883ff7510c662
ValentinCA28 <12286@holbertonstudents.com>
2026-02-09 14:06:44 +0100

    task 0 + readme + gitignore

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
-#!/usr/bin/bash/python3
+#!/usr/bin/python3
+"""Module for reading and printing file contents."""
+
 
 def read_file(filename=""):
-    {
+    """Reads a text file (UTF8) and prints it to stdout.
 
-    }
+    Args:
+        filename: The path to the file to read. Defaults to empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
