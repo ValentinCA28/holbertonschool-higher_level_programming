4ebf196aca7722880d43c236782252fcb939c05e
ValentinCA28 <12286@holbertonstudents.com>
2026-02-09 14:29:50 +0100

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines functions and classes."""
+
+
+def append_write(filename="", text=""):
+    """Function description.
+
+    Args:
+        filename: Description of filename.
+        text: Description of text.
+
+    Returns:
+        Return value description.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
