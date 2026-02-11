e8949ecda0c7397ab6f68e2c147ee716379b42ee
ValentinCA28 <12286@holbertonstudents.com>
2026-02-11 12:46:29 +0100

    task 6 docstring update

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,6 +4,12 @@
 
 import json
 
+
 def load_from_json_file(filename):
+    """Load From Json File.
+
+    Args:
+        filename (type): Description of filename.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
