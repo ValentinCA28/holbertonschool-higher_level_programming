82c919a7157f71b3517d3a7a7c602a46c4e5d30c
ValentinCA28 <12286@holbertonstudents.com>
2026-02-11 12:22:50 +0100

    task 6 docstring

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Module that defines functions and classes."""
+
 
 import json
 
 def load_from_json_file(filename):
+    """Load From Json File.
+
+    Args:
+        filename (type): Description of filename.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
