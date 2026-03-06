88a66d3cbfad0cbb817005fd3dc118a59969b613
ValentinCA28 <12286@holbertonstudents.com>
2026-03-06 21:28:19 +0100

    task 10 - update

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that prints the State object with the name passed as argument.
-
+"""Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
 """
 
 import sys
