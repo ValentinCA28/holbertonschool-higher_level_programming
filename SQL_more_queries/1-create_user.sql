961237bbb134955127897dcd39bab4e90efe35df
ValentinCA28 <12286@holbertonstudents.com>
2026-03-06 21:00:42 +0100

    task 1 - add SQL script to create user_0d_1 and grant all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create user_0d_1 and grant all privileges to it
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
