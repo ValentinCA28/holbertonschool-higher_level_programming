d00e3620da6b6ba00d9a072d6d20c396e8778de7
ValentinCA28 <12286@holbertonstudents.com>
2026-03-25 13:29:38 +0100

    Using DOMContentLoaded > script in header

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        document.querySelector('#hello').textContent = data.hello;
+    })
+    .catch(function (error) {
+    console.error('Error:', error);
+    });
+});
