8207cf6d0be4ab856aaa5588e4e15fcdba503ac2
ValentinCA28 <12286@holbertonstudents.com>
2026-05-13 08:55:56 +0200

    Refactor to_json_string function docstring and formatting

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,16 +1,5 @@
-#!/usr/bin/python3
-"""Module that defines functions and classes."""
-
 import json
 
-
 def to_json_string(my_obj):
-    """Function description.
-
-    Args:
-        my_obj: Description of my_obj.
-
-    Returns:
-        Return value description.
-    """
-    return json.dumps(my_obj)
+	"""Return the JSON string representation of an object."""
+	return json.dumps(my_obj)
